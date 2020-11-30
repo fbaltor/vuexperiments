26c68c389286726e2df9d4c77e0143abda1a8726
Felipe Baltor <fbaltor@gmail.com>
2020-11-29 23:02:25 -0300

    Simple API app.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Users />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Users from "./components/User.vue";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    Users,
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+let vm = createApp(App).mount('#app');
+console.log(vm);
